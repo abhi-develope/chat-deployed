b7365e64d591f7be9d8e4673829a55b6697b9648
Abhishek Prajapati <156572646+abhi-develope@users.noreply.github.com>
2024-08-31 19:18:06 +0530

    Update generateToken.js

--- Backend/jwt/generateToken.js
+++ Backend/jwt/generateToken.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken"
 
 const createTokenAndSaveCookie = (userId, res) => {
-    const token = jwt.sign({userId}, process.env.JWT_TOKEN,{
-        expiresIn: "10d"
+    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
+        expiresIn: "15d"
     })
     res.cookie("jwt", token, {
-        httpOnly: true, // xxs
-        secure: true,
-        sameSite: "strict" //csrf
-
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        httpOnly: true, // prevent xss attacks
+        sameSite: "strict", // prevent csrf attacks
+        secure: process.env.NODE_ENV !== "development",
     })
 }
 
